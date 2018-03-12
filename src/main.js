b599057a52d51320b566c6a9f3b9c48be0e13ed7
Alexandru Florian Barascu <alex.florin235@yahoo.com>
2018-03-12 21:39:55 +0200

    Added Vuex as a dependency.

--- src/main.js
+++ src/main.js
@@ -2,14 +2,17 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
+import Vuex from 'vuex'
 import router from './router';
 
 Vue.config.productionTip = false;
+
+Vue.use(Vuex);
 
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
     router,
-    components: {App},
+    components: { App },
     template: '<App/>'
 });
